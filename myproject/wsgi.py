041e12cc7c3308dd08739c3f7ee6b176450ac68f
P.N.S Sri vardhan <211801410005@cutmap.ac.in>
2025-02-06 06:35:25 +0000

    updated the wsgi.py

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,16 +1,9 @@
 import os
-import shutil
-import django
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
-import logging
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
 
-# Vercel requires this alias
 app = application
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-logging.debug("WSGI application initialized.")
